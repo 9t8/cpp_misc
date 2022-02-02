41d59cb0a38131d6c6cd80f2c61cceecfbae01a3
David <63334933+DavidDeLord@users.noreply.github.com>
2022-02-02 13:25:33 -0800

    update

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -5,5 +5,10 @@ using ll = long long;
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  std::cout << 1 ["as"] << '\n';
+  std::array<int, 0> a;
+  try {
+    std::cout << a.at(0) << '\n';
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
 }
