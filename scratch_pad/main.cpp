20c047218aa87805206e6ce5a6d087e670ba1be3
David <63334933+DavidDeLord@users.noreply.github.com>
2022-02-04 01:14:06 -0800

    update

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -6,9 +6,5 @@ int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
   std::array<int, 0> a;
-  try {
-    std::cout << a.at(0) << '\n';
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << '\n';
-  }
+  std::cout << a.at(0) << '\n';
 }
