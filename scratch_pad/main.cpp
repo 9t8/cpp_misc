c01f1ec1ee84d1f31f9ae7391a5e1fd09524a6c5
David <63334933+DavidDeLord@users.noreply.github.com>
2022-02-08 13:00:50 -0800

    update

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -2,9 +2,4 @@
 
 using ll = long long;
 
-int main() {
-  std::cin.tie(nullptr)->sync_with_stdio(false);
-
-  std::array<int, 0> a;
-  std::cout << a.at(0) << '\n';
-}
+int main() { std::cin.tie(nullptr)->sync_with_stdio(false); }
