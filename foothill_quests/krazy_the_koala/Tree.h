9a658be4a636574866097e85dc81ef5c22ddd841
David <63334933+DavidDeLord@users.noreply.github.com>
2022-02-15 02:32:15 -0800

    update

--- foothill_quests/krazy_the_koala/Tree.h
+++ foothill_quests/krazy_the_koala/Tree.h
@@ -8,9 +8,9 @@ class Tree {
 public:
   Tree() : _root(new Node("ROOT")) {}
 
-  ~Tree() { delete _root; }
+  Tree(const Tree &that) : _root(new Node()) { *this = that; }
 
-  Tree(const Tree &that) { *this = that; }
+  ~Tree() { delete _root; }
 
   Tree &operator=(const Tree &that) {
     *_root = *that._root;
@@ -21,19 +21,45 @@ public:
     return "# Tree rooted at " + _root->_data +
            "\n# The following lines are of the form:\n"
            "#   node: child1 child2...\n" +
-           _root->to_string() +
-           "\n" // prof is probably a fucking idiot
-           "# End of Tree\n";
+           _root->to_string() + "# End of Tree\n";
   }
 
-  void make_special_config_1(const std::vector<std::string> &names) {}
+  void make_special_config_1(const std::vector<std::string> &names) {
+    _root = new Node("ROOT");
+    _root->insert_sibling(new Node(names[0]))
+        ->insert_sibling(new Node(names[1]))
+        ->insert_sibling(new Node(names[2]))
+        ->insert_sibling(new Node(names[3]));
+    _root->_sibling->insert_child(new Node(names[4]))
+        ->insert_sibling(new Node(names[5]));
+    _root->_sibling->_sibling->insert_child(new Node(names[6]))
+        ->insert_sibling(new Node(names[7]));
+    _root->_sibling->_sibling->_sibling->insert_child(new Node(names[8]))
+        ->insert_sibling(new Node(names[9]));
+    _root->_sibling->_sibling->_sibling->_sibling
+        ->insert_child(new Node(names[10]))
+        ->insert_sibling(new Node(names[11]));
+    _root->_sibling->_child->insert_child(new Node(names[12]));
+    _root->_sibling->_child->_sibling->insert_child(new Node(names[13]));
+    _root->_sibling->_sibling->_child->insert_child(new Node(names[14]));
+    _root->_sibling->_sibling->_child->_sibling->insert_child(
+        new Node(names[15]));
+    _root->_sibling->_sibling->_sibling->_child->insert_child(
+        new Node(names[16]));
+    _root->_sibling->_sibling->_sibling->_child->_sibling->insert_child(
+        new Node(names[17]));
+    _root->_sibling->_sibling->_sibling->_sibling->_child->insert_child(
+        new Node(names[18]));
+    _root->_sibling->_sibling->_sibling->_sibling->_child->_sibling
+        ->insert_child(new Node(names[19]));
+  }
 
   bool operator==(const Tree &that) const { return *_root == *that._root; }
 
   bool operator!=(const Tree &that) const { return !(*this == that); }
 
   friend std::ostream &operator<<(std::ostream &os, const Tree &tree) {
-    return os; // anand moment
+    return os << tree.to_string();
   }
 
 private:
@@ -44,6 +70,9 @@ private:
     Node *_sibling, *_child;
 
     static bool is_equal(const Node *p1, const Node *p2) {
+      if (p1 == nullptr || p2 == nullptr)
+        return p1 == nullptr && p2 == nullptr;
+
       return p1 == p2 ||
              (p1->_data == p2->_data && is_equal(p1->_sibling, p2->_sibling) &&
               is_equal(p1->_child, p2->_child));
@@ -51,12 +80,15 @@ private:
 
     Node(std::string s = "") : _data(s), _sibling(nullptr), _child(nullptr) {}
 
-    Node(const Node &that) { *this = that; }
+    Node(const Node &that) : _sibling(nullptr), _child(nullptr) {
+      *this = that;
+    }
 
     const Node &operator=(const Node &that) {
       _data = that._data;
       if (that._sibling == nullptr) {
         delete _sibling;
+        _sibling = nullptr;
       } else {
         if (_sibling == nullptr)
           _sibling = new Node();
@@ -64,6 +96,7 @@ private:
       }
       if (that._child == nullptr) {
         delete _child;
+        _child = nullptr;
       } else {
         if (_child == nullptr)
           _child = new Node();
@@ -89,8 +122,10 @@ private:
     }
 
     Node *insert_child(Node *p) {
-      return (_child == nullptr ? this : insert_sibling(new Node()))->_child =
-                 p;
+      if (_child == nullptr)
+        return _child = p;
+
+      return _child->insert_sibling(p);
     }
 
     std::string to_string() const {
@@ -104,9 +139,10 @@ private:
         return result;
 
       result += "# Child of " + _data + "\n" + _child->to_string();
-      for (auto p_it(_child->_sibling); p_it != nullptr;
+      for (auto p_it(_child); p_it->_sibling != nullptr;
            p_it = p_it->_sibling) {
-        result += "# Next sib of " + _data + "\n" + p_it->to_string();
+        result +=
+            "# Next sib of " + p_it->_data + "\n" + p_it->_sibling->to_string();
       }
       return result;
     }
--- foothill_quests/krazy_the_koala/main.cpp
+++ foothill_quests/krazy_the_koala/main.cpp
@@ -2,4 +2,11 @@
 
 #include <bits/stdc++.h>
 
-int main() {}
+int main() {
+  Tree t;
+  t.make_special_config_1({"AABA", "ABAB", "ABBA", "BABA", "COBO",
+                           "COCO", "CODO", "COFO", "COGO", "COHO",
+                           "COJO", "COKO", "DIBI", "DIDI", "DIFI",
+                           "DIGI", "DIHI", "DIJI", "DIKI", "DILI"});
+  std::cout << t;
+}
