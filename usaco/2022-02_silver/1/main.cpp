4d2c6ef05ef1e8f934cb782e88972cb1a2514c15
David <63334933+DavidDeLord@users.noreply.github.com>
2022-03-01 03:34:32 -0800

    update

--- usaco/2022-02_silver/1/main.cpp
+++ usaco/2022-02_silver/1/main.cpp
@@ -2,4 +2,11 @@
 
 using ll = long long;
 
-int main() { std::cin.tie(nullptr)->sync_with_stdio(false); }
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+
+  ll N;
+  std::cin >> N;
+
+  std::vector<std::vector<ll>> wlists(N, std::vector<ll>(N));
+}
