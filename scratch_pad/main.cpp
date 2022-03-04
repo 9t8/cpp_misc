209a633cc4680d0b66b916a3cbba787c3074868a
David <63334933+DavidDeLord@users.noreply.github.com>
2022-03-04 12:13:37 -0800

    update

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -2,25 +2,10 @@
 
 using ll = long long;
 
-struct S {
-  S() { std::cout << "S\n"; }
-};
-
-template <typename T> struct U {
-  U() { std::cout << "U\n"; }
-};
-
-template <typename T> struct TS {
-  static inline S s = S();
-
-  static inline U<T> u = U<T>();
-
-  TS() { std::cout << "TS\n"; };
-};
-
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  TS<int> a;
-  TS<double> b;
+  ll a(1), b(2);
+  a ^= b ^= a ^= b;
+  std::cout << a << b << '\n';
 }
