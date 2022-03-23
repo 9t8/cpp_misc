67cc8f4708734596758c7fa73c1b4c5bc4e5c1cf
David <63334933+DavidDeLord@users.noreply.github.com>
2022-03-23 12:15:59 -0700

    update

--- foothill_quests/just_simply_bee/Graph.h
+++ foothill_quests/just_simply_bee/Graph.h
@@ -17,15 +17,53 @@ public:
               {}};
   }
 
-  void make_driftin_dragonfly() { _nodes = {}; }
+  void make_driftin_dragonfly() {
+    _nodes = {{{1, "dis-be-me-head"}},
+              {{2, "me"}, {5, "dis-be-me"}, {9, "dis-be-me"}},
+              {{3, "long-tail"}},
+              {{4, "be-dis"}},
+              {},
+              {{6, "front-right"}},
+              {{7, "me-right"}},
+              {{8, "back"}},
+              {{2, "be-dis"}},
+              {{10, "front-left"}},
+              {{11, "and-left"}},
+              {{12, "back"}},
+              {{2, "be-dis"}}};
+  }
 
-  void make_slinky_star() { _nodes = {}; }
+  void make_slinky_star() {
+    _nodes = {{}};
+    _nodes.resize(15);
+    for (int i(1); i < 15; i += 2) {
+      _nodes[0].push_back({i, "-"});
+      _nodes[i] = {{i + 1, "-"}};
+    }
+  }
 
-  void make_kathy_da_grate() { _nodes = {}; }
+  void make_kathy_da_grate() {
+    _nodes.clear();
+    _nodes.resize(15);
+    for (int i(0); i < 5; ++i) {
+      _nodes[i] = {{(i + 1) % 5, "-"}, {2 * i + 5, "-"}};
+      _nodes[2 * i + 5] = {{2 * i + 6, "-"}};
+    }
+  }
 
-  void make_dodos_in_space() { _nodes = {}; }
+  void make_dodos_in_space() {
+    _nodes.clear();
+    _nodes.resize(50);
+    for (int i(0); i < 50; i += 2)
+      _nodes[i] = {{i + 1, "Yippee-Dodo-#" + std::to_string(i)}};
+  }
 
-  void make_purty_pitcher() { _nodes = {}; }
+  void make_purty_pitcher() {
+    _nodes.clear();
+    int depth(3);
+    _nodes.resize(2 * powl(3, depth - 1) + 1);
+    sierpinski(depth, 0, 0);
+  }
 
 protected:
   friend class Tests;
@@ -37,9 +75,16 @@ protected:
 
   std::vector<std::vector<Edge>> _nodes;
 
-  // Suggested private helpers. Not tested.
-
-  void add_edge(int src, int dst, std::string tag);
-
-  std::string to_string() const;
+  void sierpinski(const int &depth, const int &link_root,
+                  const int &node_root) {
+    if (depth == 1) {
+      _nodes[link_root] = {{node_root + 1, ""}, {node_root + 2, ""}};
+      return;
+    }
+    sierpinski(depth - 1, link_root, node_root);
+    int root_offset(powl(3, depth - 2));
+    sierpinski(depth - 1, node_root + root_offset, node_root + 2 * root_offset);
+    sierpinski(depth - 1, node_root + 2 * root_offset,
+               node_root + 4 * root_offset);
+  }
 };
