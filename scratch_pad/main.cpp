0d8971b843a5f2db4ff35cb4aecca9350459b5e7
David <63334933+DavidDeLord@users.noreply.github.com>
2022-04-20 11:38:22 -0700

    update

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -1,12 +1,25 @@
 #include <bits/stdc++.h>
 
-class Datum {};
+class Datum {
+public:
+  virtual void eval() = 0;
+};
 
-class SimpleDatum : public Datum {};
+class SimpleDatum : public Datum {
+public:
+  virtual void eval() {}
+};
 
-class List : public Datum {};
+class List : public Datum {
+public:
+  virtual void eval() {}
+
+private:
+  std::vector<Datum *> elements;
+};
 
 int main() {
+  // combine lexing and parsing?
   while (!std::cin.eof()) {
     std::cin >> std::ws;
     if (std::cin.peek() == '(') {
