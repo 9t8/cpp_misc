b6f9ede6bba245fa4a66f596bacf054a6bdaf355
David <63334933+DavidDeLord@users.noreply.github.com>
2022-05-18 19:47:43 -0700

    bigish refactor 2

--- scratch-pad/main.cpp
+++ scratch-pad/main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-// .SECTION DATA
+// DEFINITIONS
 
 enum cat {
   ag,
@@ -27,6 +27,40 @@ enum cat {
 enum an { no3, cl, i, po4, cro4, oh, br, co3, so4, c2o4, c2h3o2, num_ans };
 
 using compound = std::pair<const cat, const an>;
+
+struct mix {
+  const compound s0, s1;
+  const bool p;
+};
+
+using evidence_table =
+    std::array<std::array<std::deque<int>, num_ans>, num_cats>;
+
+std::ostream &operator<<(std::ostream &os, const evidence_table &t) {
+  for (const auto &row : t) {
+    for (const auto &cell : row) {
+      os.width(8);
+      os << cell.size();
+    }
+    os << '\n';
+  }
+  return os;
+}
+
+void table_to_list(const std::vector<compound> &rows,
+                   const std::vector<compound> &cols,
+                   const std::vector<std::vector<bool>> &table,
+                   std::deque<mix> &list) {
+  for (size_t row{}; row < rows.size(); ++row) {
+    for (size_t col{}; col < cols.size(); ++col) {
+      if (rows[row].first != cols[col].first &&
+          rows[row].second != cols[col].second)
+        list.push_back({rows[row], cols[col], table[row][col]});
+    }
+  }
+}
+
+// DATA
 
 const std::vector<compound> s1_rows{
     {al, cl}, {ba, cl},  {k, i},     {k, po4},  {k, cro4},  {mg, cl},
@@ -67,45 +101,11 @@ const std::vector<std::vector<bool>> s3_data{
     {1, 1, 1, 0, 1, 1, 1, 1, 1}, {0, 0, 1, 0, 1, 0, 0, 1, 1},
     {0, 0, 1, 0, 1, 1, 0, 0, 1}};
 
-// XTRA DEFINITIONS
-
-struct mix {
-  const compound s0, s1;
-  const bool p;
-};
-
-using evidence_table =
-    std::array<std::array<std::deque<int>, num_ans>, num_cats>;
-
-std::ostream &operator<<(std::ostream &os, const evidence_table &t) {
-  for (const auto &row : t) {
-    for (const auto &cell : row) {
-      os.width(8);
-      os << cell.size();
-    }
-    os << '\n';
-  }
-  return os;
-}
-
-void table_to_list(const std::vector<compound> &rows,
-                   const std::vector<compound> &cols,
-                   const std::vector<std::vector<bool>> &table,
-                   std::deque<mix> &list) {
-  for (size_t row{}; row < rows.size(); ++row) {
-    for (size_t col{}; col < cols.size(); ++col) {
-      if (rows[row].first != cols[col].first &&
-          rows[row].second != cols[col].second)
-        list.push_back({rows[row], cols[col], table[row][col]});
-    }
-  }
-}
-
-// ACTUAL CODE STARTS HERE
+// MAIN CODE
 
 void gen_mix_list(std::deque<mix> &mixes) {
   mixes.clear();
-  mixes.push_back({{}, {}, false}); // mixes are one-indexed
+  mixes.push_back({{}, {}, false}); // mixes are 1-indexed, [0] used for delim
   table_to_list(s1_rows, s1_cols, s1_data, mixes);
   table_to_list(s2_rows, s2_cols, s2_data, mixes);
   table_to_list(s3_rows, s3_cols, s3_data, mixes);
@@ -123,11 +123,18 @@ void gen_soluble_evi(const std::deque<mix> &mixes,
   }
 }
 
+// TODO: MAYBE PUSH MORE EVIDENCE: TAKE ADVANTAGE OF DELIM
 void gen_insol_evi(const std::deque<mix> &mixes,
                    const evidence_table &soluble_evi,
                    evidence_table &insol_evi) {
   for (size_t i{1}; i < mixes.size(); ++i) {
     if (mixes[i].p) {
+      if (!soluble_evi[mixes[i].s0.first][mixes[i].s1.second].empty()) {
+        insol_evi[mixes[i].s1.first][mixes[i].s0.second].push_back(i);
+      }
+      if (!soluble_evi[mixes[i].s1.first][mixes[i].s0.second].empty()) {
+        insol_evi[mixes[i].s0.first][mixes[i].s1.second].push_back(i);
+      }
     }
   }
 }
