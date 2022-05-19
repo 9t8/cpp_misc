b22b1c01a1f38ce036dab7ae5af2be570925100f
David <63334933+DavidDeLord@users.noreply.github.com>
2022-05-18 18:38:04 -0700

    update

--- scratch-pad/main.cpp
+++ scratch-pad/main.cpp
@@ -18,12 +18,16 @@ enum cat {
   sn,
   cr,
   co,
-  ni
+  ni,
+  num_cats
 };
 
-enum an { no3, cl, i, po4, cro4, oh, br, co3, so4, c2o4, c2h3o2 };
+enum an { no3, cl, i, po4, cro4, oh, br, co3, so4, c2o4, c2h3o2, num_ans };
 
-using compound = std::pair<cat, an>;
+using compound = std::pair<const cat, const an>;
+
+using evidence_table =
+    std::array<std::array<std::deque<int>, num_ans>, num_cats>;
 
 struct mix {
   const compound s0, s1;
@@ -36,12 +40,14 @@ void table_to_list(const std::vector<compound> &rows,
                    std::deque<mix> &list) {
   for (size_t row{}; row < rows.size(); ++row) {
     for (size_t col{}; col < cols.size(); ++col) {
-      list.push_back({rows[row], cols[col], table[row][col]});
+      if (rows[row].first != cols[col].first &&
+          rows[row].second != cols[col].second)
+        list.push_back({rows[row], cols[col], table[row][col]});
     }
   }
 }
 
-int main() {
+void gen_mix_list(std::deque<mix> &mixes) {
   const std::vector<compound> s1_rows{
       {al, cl}, {ba, cl},  {k, i},     {k, po4},  {k, cro4},  {mg, cl},
       {na, oh}, {na, br},  {nh4, co3}, {na, so4}, {na, c2o4}, {na, c2h3o2},
@@ -82,9 +88,39 @@ int main() {
       {1, 1, 1, 0, 1, 1, 1, 1, 1}, {0, 0, 1, 0, 1, 0, 0, 1, 1},
       {0, 0, 1, 0, 1, 1, 0, 0, 1}};
 
-  std::deque<mix> mixes{mix{{}, {}, false}}; // 0 = "null" mix: used for given
+  mixes.clear();
+  mixes.push_back({{}, {}, false}); // mixes are one-indexed
   table_to_list(s1_rows, s1_cols, s1_data, mixes);
   table_to_list(s2_rows, s2_cols, s2_data, mixes);
+  table_to_list(s3_rows, s3_cols, s3_data, mixes);
+}
+
+void gen_sol_evi(const std::deque<mix> &mixes, evidence_table &sol_evi) {
+  for (size_t i{1}; i < mixes.size(); ++i) {
+    const mix &curr_mix{mixes[i]};
+    if (!curr_mix.p) {
+      sol_evi[curr_mix.s0.first][curr_mix.s0.second].push_back(i);
+      sol_evi[curr_mix.s0.first][curr_mix.s1.second].push_back(i);
+      sol_evi[curr_mix.s1.first][curr_mix.s0.second].push_back(i);
+      sol_evi[curr_mix.s1.first][curr_mix.s1.second].push_back(i);
+    }
+  }
+}
+
+int main() {
+  std::deque<mix> mixes;
+  gen_mix_list(mixes);
+
+  evidence_table sol_evi;
+  gen_sol_evi(mixes, sol_evi);
+
+  for (const auto &row : sol_evi) {
+    for (const auto &cell : row) {
+      std::cout.width(4);
+      std::cout << cell.size();
+    }
+    std::cout << "\n";
+  }
 
   std::cout << "DONE\n";
 }
