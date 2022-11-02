1892af304c8b1e47c9fbb086c71e9290bbf45b55
9t8 <63334933+9t8@users.noreply.github.com>
2022-11-01 20:45:40 -0700

    renaming

--- random_parking/main.cpp
+++ random_parking/main.cpp
@@ -5,8 +5,8 @@ int sim_rand_parking(const double &width) {
   std::map<double, double> valid_itvls{{0, 0}};
 
   int cars_parked(0);
-  for (double ar_valid_itvls(width - 1); ar_valid_itvls >= 0;) {
-    // ar_valid_itvls := S(is_valid_pos(x), 0, width)
+  for (double area_valid_itvls(width - 1); area_valid_itvls >= 0;) {
+    // area_valid_itvls := S(valid_pos?(x), 0, width)
 
     // insert new car
     static std::mt19937_64 rng; // deterministic
@@ -15,21 +15,20 @@ int sim_rand_parking(const double &width) {
     double ar_valid_itvls_before(
         dist(rng,
              decltype(dist)::param_type(
-                 0, // ar_valid_itvls
-                 // mitigates https://github.com/llvm/llvm-project/issues/19141
-                 // replace with below on unbugged compilers, otherwise ub!
-                 std::nextafter(ar_valid_itvls, ar_valid_itvls + 1))));
+                 0, // area_valid_itvls // use on llvm:
+                    // https://github.com/llvm/llvm-project/issues/19141
+                 std::nextafter(area_valid_itvls, area_valid_itvls + 1))));
 
     auto it_itvl(valid_itvls.lower_bound(ar_valid_itvls_before));
     double itvl_size(std::next(it_itvl) == valid_itvls.end()
-                         ? ar_valid_itvls - it_itvl->first
+                         ? area_valid_itvls - it_itvl->first
                          : std::next(it_itvl)->first - it_itvl->first);
 
     double left_space(it_itvl->second + ar_valid_itvls_before - it_itvl->first);
 
     double right_space(it_itvl->second + itvl_size - ar_valid_itvls_before);
 
-    double lost_ar(std::min(left_space, 1.) + std::min(right_space, 1.));
+    double lost_area(std::min(left_space, 1.) + std::min(right_space, 1.));
 
     // TODO? shorten/delete current itvl
     if (left_space >= 1) {
@@ -40,9 +39,9 @@ int sim_rand_parking(const double &width) {
       // TODO: insert new itvl
     }
 
-    ar_valid_itvls = -1;
+    area_valid_itvls = -1;
   }
   return cars_parked;
 }
 
-int main() { std::cout << sim_rand_parking(1) << "\n"; }
+int main() { std::cout << sim_rand_parking(.999) << "\n"; }
