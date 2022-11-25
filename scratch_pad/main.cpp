02df332837c217fef0c7ba51b7b27395436eea2f
9t8 <63334933+9t8@users.noreply.github.com>
2022-11-24 21:10:40 -0800

    correct template format

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -2,7 +2,10 @@
 
 using ll = long long;
 
-template <typename t, size_t sz> t &last(t (&array)[sz]) { return array[sz - 1]; }
+template <typename t, size_t sz>
+t &last(t (&super_duper_long_variable_name_for_an_array)[sz]) {
+  return super_duper_long_variable_name_for_an_array[sz - 1];
+}
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
