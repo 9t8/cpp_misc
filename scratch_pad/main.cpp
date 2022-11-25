20f0d40e9ba5c6d0ce1243dc5dc5cb579c009462
9t8 <63334933+9t8@users.noreply.github.com>
2022-11-24 21:04:58 -0800

    language server is garbage

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -2,7 +2,10 @@
 
 using ll = long long;
 
+template <typename t, size_t sz> t &last(t (&array)[sz]) { return array[sz - 1]; }
+
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  std::cout << "hello, world\n";
+  int arr[]{0, 1, 2, 3};
+  std::cout << last(arr) << "\n";
 }
