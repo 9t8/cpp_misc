4c96e263b9a745542e80b1a5336dae4058bef85e
9t8 <63334933+9t8@users.noreply.github.com>
2022-11-24 21:05:14 -0800

    newline

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -6,6 +6,7 @@ template <typename t, size_t sz> t &last(t (&array)[sz]) { return array[sz - 1];
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
+
   int arr[]{0, 1, 2, 3};
   std::cout << last(arr) << "\n";
 }
