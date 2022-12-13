55a879071b9fa1a0f04497fcc625072bcdeff6d4
9t8 <63334933+9t8@users.noreply.github.com>
2022-12-12 23:55:45 -0800

    fixed building

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -10,6 +10,6 @@ t &last(t (&super_duper_long_variable_name_for_an_array)[sz]) {
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  int arr[]{0, 1, 2, 3};
+  int arr[]{0, 1, 2};
   std::cout << last(arr) << "\n";
 }
