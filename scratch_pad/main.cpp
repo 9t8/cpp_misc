22d480d5903dc8cc38b13c4b9659964b80c81cc3
9t8 <63334933+9t8@users.noreply.github.com>
2022-12-28 02:04:46 -0800

    no idea what im doing

--- scratch_pad/main.cpp
+++ scratch_pad/main.cpp
@@ -2,27 +2,11 @@
 
 typedef long long ll;
 
-constexpr bool any_factors(ll target, ll start, ll step) {
-  if (start * start * 36 > target) {
-    return false;
-  }
-
-  if (step == 1) {
-    return target % (start * 6 + 1) == 0 || target % (start * 6 + 5) == 0;
-  }
-
-  return any_factors(target, start, step / 2) ||
-         any_factors(target, start + step / 2, (step + 1) / 2);
-}
-
-constexpr bool is_prime(ll target) {
-  return target == 2 || target == 3 || target == 5 ||
-         (target > 1 && target % 2 != 0 && target % 3 != 0 && target % 5 != 0 &&
-          !any_factors(target, 1, target / 6 + 1));
-}
-
-int main() {
+int main(int argc, const char *[]) {
   std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  std::cout << is_prime(111111111111111111) << "\n";
+  int k(0x7fffffff);
+  k += argc;
+
+  std::cout << k << "\n";
 }
