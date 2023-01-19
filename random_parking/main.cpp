b0515dc9c9d2844ccf7ac8212e52e831cccdd4e7
9t8 <63334933+9t8@users.noreply.github.com>
2023-01-19 04:23:06 -0800

    make prefix list correct

--- random_parking/main.cpp
+++ random_parking/main.cpp
@@ -6,43 +6,39 @@ int sim_rand_parking(const double &width) {
   int cars_parked(0);
 
   // prefix sum of preceding valid area for each itvl
-  std::vector<double> itvl_prefixes({0});
+  std::vector<double> itvl_prefixes({width - 1});
 
-  // valid_area = S_{0}^{width} valid_pos?(x)
-  for (double valid_area(width - 1); valid_area >= 0; ++cars_parked) {
+  for (; !itvl_prefixes.empty(); ++cars_parked) {
     // generate random location for new car
     static std::mt19937_64 rng;  // currently deterministic
     static std::uniform_real_distribution<> dist;
     double new_loc(dist(rng,
 #ifdef __clang__
                         // https://github.com/llvm/llvm-project/issues/19141
-                        decltype(dist)::param_type(0, valid_area)
+                        decltype(dist)::param_type(0, itvl_prefixes.back())
 #else
                         decltype(dist)::param_type(
-                            0, std::nextafter(valid_area, valid_area + 1))
+                            0, std::nextafter(itvl_prefixes.back(),
+                                              itvl_prefixes.back() + 1))
 #endif
                             ));
 
-    auto selected_itvl(
-        lower_bound(itvl_prefixes.begin(), itvl_prefixes.end(), new_loc));
+    auto selected_itvl(lower_bound(itvl_prefixes.begin(), itvl_prefixes.end(),
+                                   new_loc));  // FIX
 
-    double itvl_end(selected_itvl + 1 == itvl_prefixes.end()
-                        ? valid_area
-                        : selected_itvl[1]),
-        left_offset(new_loc - *selected_itvl), right_offset(itvl_end - new_loc);
-    assert(new_loc >= 0);
-    assert(left_offset >= 0);
-    assert(right_offset >= 0);
+    double itvl_start(
+        selected_itvl == itvl_prefixes.begin() ? 0 : selected_itvl[-1]),
+        left_offset(new_loc - itvl_start),
+        right_offset(*selected_itvl - new_loc);
 
     // insert new car
     if (left_offset < 1 && right_offset < 1) {
       // remove interval
-      double itvl_len(itvl_end - *selected_itvl);
       while (++selected_itvl < itvl_prefixes.end()) {
-        selected_itvl[-1] = *selected_itvl - itvl_len;
+        selected_itvl[-1] = itvl_start;
       }
       itvl_prefixes.pop_back();
-      valid_area -= itvl_len;
+      valid_area -= *selected_itvl - itvl_start;
       continue;
     }
     if (left_offset >= 1 && right_offset >= 1) {
@@ -62,7 +58,6 @@ int sim_rand_parking(const double &width) {
     while (++selected_itvl < itvl_prefixes.end()) {
       *selected_itvl -= lost_area;
     }
-    valid_area -= lost_area;
   }
   return cars_parked;
 }
